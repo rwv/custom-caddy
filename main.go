f28495b9a7e51ca7315c97af3170fa86702c69e7
Seedgou <i@zczc.cz>
2022-10-24 14:17:53 +0000

    init caddy

---
+++ main.go
@@ -0,0 +1,13 @@
+// See https://github.com/caddyserver/caddy/blob/master/cmd/caddy/main.go
+package main
+
+import (
+	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+
+	// plug in Caddy modules here
+	_ "github.com/caddyserver/caddy/v2/modules/standard"
+)
+
+func main() {
+	caddycmd.Main()
+}
